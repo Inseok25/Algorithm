f5c376be32af7e32b9930f75226a94727ba30015
Inseok25 <Inseok25@gmail.com>
2025-05-28 19:48:40 +0900

    Stack 연습 및 구현

---
+++ Stack/Stack.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <stack>
+#include <vector>
+#include <list>
+using namespace std;
+
+template<typename T, typename Container = vector<T>>
+class Stack
+{
+public:
+    Stack()
+    {
+
+    }
+    void push(const T& value)
+    {
+        _container.push_back(value);
+    }
+    void pop()
+    {
+        _container.pop_back();
+    }
+    T& top()
+    {
+        return _container.back();
+    }
+    bool empty() { return _container.empty(); }
+    int size() { return _container.size(); }
+
+private:
+    Container _container;
+};
+int main()
+{
+    Stack<int, list<int>> s;
+
+    s.push(1);
+    s.push(2);
+    s.push(3);
+
+    while (s.empty() == false)
+    {
+        int data = s.top();
+
+        s.pop();
+
+        cout << data << endl;
+    }
+
+    int size = s.size();
+}
