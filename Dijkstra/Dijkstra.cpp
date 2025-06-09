140b81caaeb0000e05327768d59f2f0fa42b7464
Inseok25 <Inseok25@gmail.com>
2025-06-09 20:29:27 +0900

    트리 기초 구현

---
+++ Dijkstra/Dijkstra.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+struct Vertex
+{
+	// data
+};
+
+vector<Vertex> verticis;
+vector<vector<int>> adj;
+
+void CreateGraph()
+{
+	verticis.resize(6);
+	adj = vector<vector<int>>(6, vector<int>(6, -1));
+
+	adj[0][1] = 15;
+	adj[0][3] = 35;
+	adj[1][0] = 15;
+	adj[1][2] = 5;
+	adj[1][3] = 10;
+	adj[1][3] = 10;
+	adj[3][4] = 5;
+	adj[5][4] = 5;
+}
+
+void Dijstra(int here)
+{
+	struct VertexCost
+	{
+		int vertex;
+		int cost;
+	};
+
+	list<VertexCost> discovered;
+	vector<int> best(6, INT32_MAX);
+	vector<int> parant(6, -1);
+
+	discovered.push_back(VertexCost{ here, 0 });
+	best[here] = 0;
+	parant[here] = here;
+
+	while (discovered.empty() == false)
+	{
+		list<VertexCost>::iterator bestIt;
+		int bestCost = INT32_MAX;
+		for (auto it = discovered.begin(); it != discovered.end();it++)
+		{
+			if (it->cost < bestCost)
+			{
+				bestCost = it->cost;
+				bestIt = it;
+			}
+		}
+
+		int cost = bestIt->cost;
+		here = bestIt->vertex;
+		discovered.erase(bestIt);
+
+		if (best[here] < cost)
+			continue;
+
+		for (int there = 0; there < 6; there++)
+		{
+			if (adj[here][there] == -1)
+				continue;
+
+			int nextCost = best[here] + adj[here][there];
+			if (nextCost >= best[there])
+				continue;
+
+			discovered.push_back(VertexCost{ there, nextCost });
+
+			best[there] = nextCost;
+			parant[there] = here;
+
+		}
+	}
+}
+int main()
+{
+	CreateGraph();
+	return 0;
+}
---
+++ Tree/Tree.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+using NodeRef = shared_ptr<struct Node>;
+
+struct Node
+{
+    Node() {};
+    Node(const string& data) : data(data) {};
+    string          data;
+    vector<NodeRef> child;
+};
+
+NodeRef CreateTree()
+{
+    NodeRef root = make_shared<Node>("R1 개발실");
+    {
+        NodeRef node = make_shared<Node>("디자인팀");
+        root->child.push_back(node);
+        {
+            NodeRef leaf = make_shared<Node>("전투");
+            node->child.push_back(leaf);
+        }
+        {
+            NodeRef leaf = make_shared<Node>("경제");
+            node->child.push_back(leaf);
+        }
+        {
+            NodeRef leaf = make_shared<Node>("스토리");
+            node->child.push_back(leaf);
+        }
+    }
+
+    return root;
+}
+
+void PrintTree(NodeRef root,int depth)
+{
+    for (int i = 0; i < depth; i++)
+    {   
+        cout << "-";
+    }
+    cout << root->data << endl;
+
+    for (NodeRef& child : root->child)
+        PrintTree(child, depth+1);
+}
+
+int GetHeight(NodeRef root)
+{
+    int height = 1;
+
+    for (NodeRef& child : root->child)
+        height = max(height, GetHeight(child) + 1);
+
+    return height;
+}
+int main()
+{
+    NodeRef root = CreateTree();
+
+    PrintTree(root, 0);
+
+    cout << GetHeight(root) << endl;
+}
