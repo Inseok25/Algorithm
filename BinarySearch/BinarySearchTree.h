632d845197f45434f1984c83d44b98a5c656548f
Inseok25 <Inseok25@gmail.com>
2025-06-11 20:01:37 +0900

    RED BLACK TREE 실습

--- BinarySearch/BinarySearchTree.h
+++ BinarySearch/BinarySearchTree.h
@@ -31,7 +31,7 @@ public:
 	BinarySearchTree();
 	~BinarySearchTree();
 
-	void	Print() { Print(_root, 10, 0); }
+	void	Print();
 	void	Print(Node* node, int x, int y);
 
 	Node* Search(Node* node, int key);
@@ -45,6 +45,8 @@ public:
 
 	void	Delete(int key);
 	void	Delete(Node* node);
+	void	DeleteFixup(Node* node);
+
 	void	Replace(Node* u, Node* v);
 
 	// Red-Black Tree
