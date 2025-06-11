bcfd46203c9dbf591a804023dad75ded35f30799
Inseok25 <Inseok25@gmail.com>
2025-06-12 02:30:48 +0900

    퀵 정렬 추가 실습

--- Sort/Sort.cpp
+++ Sort/Sort.cpp
@@ -192,6 +192,41 @@ void MergeSort(vector<int>& v, int left, int right)
 	MergeResult(v, left, mid, right);
 }
 
+int Partition(vector<int>& v, int left, int right)
+{
+	int pivot = v[left];
+	int low = left + 1;
+	int high = right;
+
+	// O(N)
+	while (low <= high)
+	{
+		while (low <= right && pivot >= v[low])
+			low++;
+
+		while (high >= left + 1 && pivot <= v[high])
+			high--;
+
+		if (low < high)
+			swap(v[low], v[high]);
+	}
+
+	swap(v[left], v[high]);
+	return high;
+}
+
+// O(N^2) < 최악
+// O(NlogN) < 평균
+void QuickSort(vector<int>& v, int left, int right)
+{
+	if (left > right)
+		return;
+
+	int pivot = Partition(v, left, right);
+	QuickSort(v, left, pivot - 1);
+	QuickSort(v, pivot + 1, right);
+}
+
 int main()
 {
 	vector<int> v;
@@ -208,7 +243,8 @@ int main()
 	//InsertionSort(v);
 
 	//HeapSort(v);
-	MergeSort(v, 0, v.size() - 1);
+	//MergeSort(v, 0, v.size() - 1);
+	QuickSort(v, 0, v.size() - 1);
 
 
 }
