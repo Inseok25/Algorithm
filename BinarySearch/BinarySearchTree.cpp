79f3bba6d3bc6a1a2d7ed760e73d94d21a7d9167
Inseok25 <Inseok25@gmail.com>
2025-06-11 17:11:10 +0900

    RED BLACK TREE 실습

--- BinarySearch/BinarySearchTree.cpp
+++ BinarySearch/BinarySearchTree.cpp
@@ -3,103 +3,218 @@
 #include <windows.h>
 using namespace std;
 
+enum class ConsoleColor
+{
+	BLACK = 0,
+	RED = FOREGROUND_RED,
+	GREEN = FOREGROUND_GREEN,
+	BLUE = FOREGROUND_BLUE,
+	YELLOW = RED | GREEN,
+	WHITE = RED | GREEN | BLUE,
+};
+
+void SetCursorColor(ConsoleColor color)
+{
+	HANDLE output = ::GetStdHandle(STD_OUTPUT_HANDLE);
+	::SetConsoleTextAttribute(output, static_cast<SHORT>(color));
+}
+
 void SetCursorPosition(int x, int y)
 {
 	HANDLE output = ::GetStdHandle(STD_OUTPUT_HANDLE);
 	COORD pos = { static_cast<SHORT>(x), static_cast<SHORT>(y) };
 	::SetConsoleCursorPosition(output, pos);
 }
+
+BinarySearchTree::BinarySearchTree()
+{
+	_nil = new Node(); // Black
+	_root = _nil;
+}
+
+BinarySearchTree::~BinarySearchTree()
+{
+	delete _nil;
+}
+
 void BinarySearchTree::Print(Node* node, int x, int y)
 {
-	if (node == nullptr)
+	if (node == _nil)
 		return;
 
 	SetCursorPosition(x, y);
+
+	if (node->color == Color::Black)
+		SetCursorColor(ConsoleColor::BLUE);
+	else
+		SetCursorColor(ConsoleColor::RED);
+
 	cout << node->key;
 	Print(node->left, x - (5 / (y + 1)), y + 1);
 	Print(node->right, x + (5 / (y + 1)), y + 1);
-}
 
-void BinarySearchTree::Print_Inorder(Node* node)
-{
-
-	if (node == nullptr)
-		return;
-
-	cout << node->key << endl;
-	Print_Inorder(node->left);
-	Print_Inorder(node->right);
+	SetCursorColor(ConsoleColor::WHITE);
 }
 
 Node* BinarySearchTree::Search(Node* node, int key)
 {
-	if (node == nullptr || key == node->key)
+	if (node == _nil || key == node->key)
 		return node;
 
 	if (key < node->key)
 		return Search(node->left, key);
 	else
 		return Search(node->right, key);
-	
 }
 
 Node* BinarySearchTree::Min(Node* node)
 {
-	while (node->left)
+	while (node->left != _nil)
 		node = node->left;
+
 	return node;
 }
 
 Node* BinarySearchTree::Max(Node* node)
 {
-	while (node->right)
+	while (node->right != _nil)
 		node = node->right;
+
 	return node;
 }
 
 Node* BinarySearchTree::Next(Node* node)
 {
-	if (node->right)
+	if (node->right != _nil)
 		return Min(node->right);
-	Node* parant = node->parant;
 
-	while (parant && node == parant->right)
+	Node* parent = node->parent;
+
+	while (parent != _nil && node == parent->right)
 	{
-		node = parant;
-		parant = parant->parant;
+		node = parent;
+		parent = parent->parent;
 	}
 
-	return parant;
+	return parent;
 }
 
-void BinarySearchTree::insert(int key)
+void BinarySearchTree::Insert(int key)
 {
 	Node* newNode = new Node();
 	newNode->key = key;
 
-	if (_root == nullptr)
-	{
-		_root = newNode;
-		return;
-	}
-
 	Node* node = _root;
-	Node* parant = nullptr;
+	Node* parent = _nil;
 
-	while (node)
+	while (node != _nil)
 	{
-		parant = node;
+		parent = node;
 		if (key < node->key)
 			node = node->left;
 		else
 			node = node->right;
 	}
 
-	newNode->parant = parant;
-	if (key < parant->key)
-		parant->left = newNode;
+	newNode->parent = parent;
+
+	if (parent == _nil)
+		_root = newNode;
+	else if (key < parent->key)
+		parent->left = newNode;
 	else
-		parant->right = newNode;
+		parent->right = newNode;
+
+	// 검사
+	newNode->left = _nil;
+	newNode->right = _nil;
+	newNode->color = Color::Red;
+
+	InsertFixup(newNode);
+}
+
+void BinarySearchTree::InsertFixup(Node* node)
+{
+	// 1) p = red, uncle = red
+	// -> p = black, uncle = black, pp = red로 바꿈
+	// 2) p = red, uncle = black (triangle)
+	// -> 회전을 통해 case 3으로 바꿈
+	// 3) p = red, uncle = black (list)	
+	// -> 색상 변경 + 회전
+
+	while (node->parent->color == Color::Red)
+	{
+		if (node->parent == node->parent->parent->left)
+		{
+			Node* uncle = node->parent->parent->right;
+			if (uncle->color == Color::Red)
+			{
+				node->parent->color = Color::Black; // p
+				uncle->color = Color::Black; // u
+				node->parent->parent->color = Color::Red; // pp
+				node = node->parent->parent;
+			}
+			else
+			{
+				//       [pp(B)]
+				//   [p(R)]     [u(B)]
+				//      [n(R)]
+
+				//        [pp(B)]
+				//      [p(R)]  [u(B)]
+				//   [n(R)]   
+
+				if (node == node->parent->right) // Triangle 타입
+				{
+					node = node->parent;
+					LeftRotate(node);
+				}
+
+				// List 타입
+
+				//        [pp(R)]
+				//      [p(B)]  [u(B)]
+				//   [n(R)]  
+
+				//       [p(B)]  
+				//   [n(R)]   [pp(R)]
+				//					[u(B)]
+				node->parent->color = Color::Black;
+				node->parent->parent->color = Color::Red;
+				RightRotate(node->parent->parent);
+			}
+		}
+		else
+		{
+			Node* uncle = node->parent->parent->left;
+			if (uncle->color == Color::Red)
+			{
+				node->parent->color = Color::Black; // p
+				uncle->color = Color::Black; // u
+				node->parent->parent->color = Color::Red; // pp
+				node = node->parent->parent;
+			}
+			else
+			{
+				if (node == node->parent->left) // Triangle 타입
+				{
+					node = node->parent;
+					RightRotate(node);
+				}
+
+				// List 타입
+
+				//					 [p(B)]    
+				//			  [pp(R)]      [n(R)]  
+				//      [u(B)] 
+				node->parent->color = Color::Black;
+				node->parent->parent->color = Color::Red;
+				LeftRotate(node->parent->parent);
+			}
+		}
+	}
+
+	_root->color = Color::Black;
 }
 
 void BinarySearchTree::Delete(int key)
@@ -119,25 +234,85 @@ void BinarySearchTree::Delete(Node* node)
 		Replace(node, node->left);
 	else
 	{
+		// 다음 데이터 찾기
 		Node* next = Next(node);
 		node->key = next->key;
 		Delete(next);
 	}
 }
 
-//u 서브트리를 v 서브트리로 교체
-//그리고 delete u
+// u 서브트리를 v 서브트리로 교체
+// 그리고 delete u
 void BinarySearchTree::Replace(Node* u, Node* v)
 {
-	if (u->parant == nullptr)
+	if (u->parent == nullptr)
 		_root = v;
-	else if (u == u->parant->left)
-		u->parant->left = v;
+	else if (u == u->parent->left)
+		u->parent->left = v;
 	else
-		u->parant->right = v;
+		u->parent->right = v;
 
 	if (v)
-		v->parant = u->parant;
+		v->parent = u->parent;
 
 	delete u;
+}
+//     [y]
+//  [x]   [3]
+// [1][2]
+
+//    [x]  
+// [1]   [y]
+//      [2][3]
+
+void BinarySearchTree::LeftRotate(Node* x)
+{
+	Node* y = x->right;
+
+	x->right = y->left; // [2];
+
+	if (y->left != _nil)
+		y->left->parent = x;
+
+	y->parent = x->parent;
+
+	if (x->parent == _nil)
+		_root = y;
+	else if (x == x->parent->left)
+		x->parent->left = y;
+	else
+		x->parent->right = y;
+
+	y->left = x;
+	x->parent = y;
+}
+
+//     [y]
+//  [x]   [3]
+// [1][2]
+
+//    [x]  
+// [1]   [y]
+//      [2][3]
+
+void BinarySearchTree::RightRotate(Node* y)
+{
+	Node* x = y->left;
+
+	y->left = x->right; // [2];
+
+	if (x->right != _nil)
+		x->right->parent = y;
+
+	x->parent = y->parent;
+
+	if (y->parent == _nil)
+		_root = x;
+	else if (y == y->parent->left)
+		y->parent->left = x;
+	else
+		y->parent->right = x;
+
+	x->right = y;
+	y->parent = x;
 }
--- BinarySearch/BinarySearchTree.h
+++ BinarySearch/BinarySearchTree.h
@@ -1,19 +1,38 @@
 #pragma once
 
+//     [10]
+//	 [5] [20]
+//	       [30] 	
+
+enum class Color
+{
+	Red = 0,
+	Black = 1,
+};
+
 struct Node
 {
-	Node*	parant = nullptr;
-	Node*	left = nullptr;
-	Node*	right = nullptr;
+	Node* parent = nullptr;
+	Node* left = nullptr;
+	Node* right = nullptr;
 	int		key = {};
+	Color	color = Color::Black;
 };
+
+// Red-Black Tree
+// 1) 모든 노드는 Red or Black
+// 2) Root는 Black
+// 3) Leaf(NIL)는 Black
+// 4) Red 노드의 자식은 Black (연속해서 Red-Red X)
+// 5) 각 노드로부터 ~ 리프까지 가는 경로들은 모두 같은 수의 Black
 class BinarySearchTree
 {
 public:
-	void Print() { Print(_root, 10, 0); }
-	void Print(Node* node, int x, int y);
-	void Print_Inorder() { Print_Inorder(_root); }
-	void Print_Inorder(Node* node);
+	BinarySearchTree();
+	~BinarySearchTree();
+
+	void	Print() { Print(_root, 10, 0); }
+	void	Print(Node* node, int x, int y);
 
 	Node* Search(Node* node, int key);
 
@@ -21,14 +40,19 @@ public:
 	Node* Max(Node* node);
 	Node* Next(Node* node);
 
-	void insert(int key);
+	void	Insert(int key);
+	void	InsertFixup(Node* node);
 
-	void Delete(int key);
-	void Delete(Node* node);
+	void	Delete(int key);
+	void	Delete(Node* node);
+	void	Replace(Node* u, Node* v);
 
-	void Replace(Node* u, Node* v);
+	// Red-Black Tree
+	void	LeftRotate(Node* node);
+	void	RightRotate(Node* node);
 
 private:
 	Node* _root = nullptr;
+	Node* _nil = nullptr;
 };
 
